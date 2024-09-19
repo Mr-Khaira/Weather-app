6589d3c56dcaffd384070264bf9bc6a803ab2b40
Mr Khaira <singhkhairaharkaran@gmail.com>
2024-09-19 09:56:50 -0400

    pooject redy for deployment.

--- utilities/helperFunctions.js
+++ utilities/helperFunctions.js
@@ -1,16 +1,3 @@
-/******************************************************************************
- ***
- * WEB422 – Assignment 1
- * I declare that this assignment is my own work in accordance with Seneca Academic Policy.
- * No part of this assignment has been copied manually or electronically from any other source
- * (including web sites) or distributed to other students.
- *
- * Name: Harkaran Singh Khaira Student ID: 170321210 Date: 28/05/2024
- *
- *
- ******************************************************************************
- **/
-
 export function searchError() {
   const brTag = document.createElement("br");
   searchBar.appendChild(brTag);
@@ -66,7 +53,6 @@ export function epochToLocalTime(epochTime) {
   }
 
   const min = baseTime.getMinutes();
-  // Print the result
   return `${hour}:${min}${amPm} ${date}-${month}-${year}`;
 }
 
